ce8e937e7df6094ca14f0e96500d702b23d8e111
kyedonghwan-linux <kdh643737@gmail.com>
2020-10-19 21:47:23 +0900

    20201019

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+const app = express();
+
+app.set('view engine', "pug");
+//원하는 만큼 middleware를 선언 middelware 처리 후, route로 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());//보안을 위함
+app.use(morgan("dev"));
+
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
